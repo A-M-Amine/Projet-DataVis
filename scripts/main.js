6f55b82ea0c2580fffd6b3eb8a54849b2c3854a3
amine <ahmane.m.amine@gmail.com>
2022-12-03 02:27:21 +0100

    fixed zooming jitter

--- scripts/main.js
+++ scripts/main.js
@@ -34,25 +34,42 @@ function drawMap(semester_Nb, day_Str) {
         .projection(proj);
 
 
-    // attach svg to body
-    let svg = d3.select("body")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("class", "map");
 
 
 
-    d3.json("data/mapUSTHBen.geojson", function (json) {
-        d3.json("data/MIV-M2-S1.JSON", function (prog) {
+
+    d3.json("data/USTHB_V11.geojson", function (json) {
+        d3.json("data/M2_MIV.JSON", function (prog) {
 
 
             // scaling parameters
             let b = path.bounds(json);
-            s = .99 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h);
+            s = 0.95 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h);
             t = [(w - s * (b[1][0] + b[0][0])) / 2, (h - s * (b[1][1] + b[0][1])) / 2];
+
             proj.translate(t).scale(s);
 
+            // used to smooth zooming
+            let zoomSvg = d3.select("body").append('g')
+            
+
+            // attach svg to body
+            let svg = zoomSvg.append("svg")
+                .attr("width", w)
+                .attr("height", h)
+                .attr("class", "map");
+
+
+            // zoom + pan
+            let zoom = d3.behavior.zoom()
+                .scaleExtent([1, 10])
+                .on("zoom", function () {
+                    svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+
+            });
+
+
+            zoomSvg.call(zoom);
 
 
             // functions
@@ -63,19 +80,19 @@ function drawMap(semester_Nb, day_Str) {
                     if (Object.keys(day[ele].cours).length != 0) {
                         id = day[ele].cours.loc
                         d3.select("[id='" + id + "']")
-                            .style("fill", "red");
+                            .style("fill", "#dc3545");
 
                     } else {
                         if (day[ele].groups.hasOwnProperty('G1')) {
                             id = day[ele].groups.G1.loc
 
                             d3.select("[id='" + id + "']")
-                                .style("fill", "red");
+                                .style("fill", "#dc3545");
                         }
                         if (day[ele].groups.hasOwnProperty('G2')) {
                             id = day[ele].groups.G2.loc
                             d3.select("[id='" + id + "']")
-                                .style("fill", "green");
+                                .style("fill", "#dc3545");
                         }
 
                     }
@@ -156,6 +173,15 @@ function drawMap(semester_Nb, day_Str) {
                 return res
             }
 
+            //TODO change color based on location floor 
+            // function checkSalleTDFloor(loc) {
+
+            //     if(!isNaN(loc)) {
+            //         let number = parseInt(loc) 
+            //         if(number >= 200 && number < 300) || (number >= )
+            //     }
+            // }
+
 
             // creating map            
             svg.selectAll("path")
@@ -174,7 +200,7 @@ function drawMap(semester_Nb, day_Str) {
                 .on("mouseout", mouseOff);
 
 
-            //TODO make days and semestre change by user input
+            
             day = prog.days[day_Str];
 
             highlightDay(day);
@@ -185,15 +211,5 @@ function drawMap(semester_Nb, day_Str) {
     });
 
 
-
-    // zoom + pan
-    let zoom = d3.behavior.zoom()
-        .on("zoom", function () {
-            svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
-
-        });
-
-
-    svg.call(zoom);
 
 }
