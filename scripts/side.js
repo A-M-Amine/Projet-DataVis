f6b030d250cea5821ca4e51c9365bac56804e17b
amine <ahmane.m.amine@gmail.com>
2022-12-04 03:53:31 +0100

    added unique dynamic Teacher filter functions

--- scripts/side.js
+++ scripts/side.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let M1 = document.getElementById("M1");
-    let M2 = document.getElementById("M2");
 
     
     let listInner = document.getElementsByName("masterIv");
@@ -26,24 +24,146 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function showGenre(item) {
-    document.getElementById("navbarDropdown").innerHTML = item.innerHTML;
+    document.getElementById("daysDropdown").innerHTML = item.innerHTML;
+}
+
+function showTeacherName(item) {
+    document.getElementById("teacherFilterDropdown").innerHTML = item.innerHTML;
 }
 
 
 function createSelection() {
 
-    let day = document.getElementById("navbarDropdown").innerHTML;
-    let year = ""
+    let day = document.getElementById("daysDropdown").innerHTML;
+    
+    let level = "M1"
     document.getElementsByName("masterIv").forEach(element => {
         if( element.getAttribute("flag") == "1") {
-            year = element.id
+            level = element.id
         }
     });
+
+    let levelArray = ["data/M1_MIV.JSON","data/M2_MIV.JSON"]
+    let choice = levelArray[0]
+    if(level == "M1") {
+        choice = levelArray[0]
+    }else {
+        choice = levelArray[1]
+    }
+
     
-    document.getElementById("frame").src = "viz.html?data=" +  day.slice(0,3) + "=" + year
+    clearTeachers();
+    populateTeachers(choice, day.slice(0,3));
+
+    let filterinput = document.getElementById("teacherFilterDropdown").innerHTML.split(" ").join("");
+
+
+    document.getElementById("frame").src = "viz.html?data=" +  day.slice(0,3) + "=" + choice + "=" + filterinput
 
 
 }
 
 
 
+function populateTeachers(data, day) {
+
+    fetch(data)
+        .then((response) => response.json())
+        .then((data) => {
+
+            let teachers = new Set()
+
+            data.days[day].forEach(element => {
+
+                if (Object.keys(element.cours).length != 0) {
+                    
+                    if (element.cours.prof.length != 0) {
+                        teachers.add(element.cours.prof)
+                    }
+                    
+
+                } else {
+                    if (element.groups.hasOwnProperty('G1')) {
+
+                        if(element.groups.G1.prof.length != 0) {
+                            teachers.add(element.groups.G1.prof)
+                        }
+
+                    }
+                    if (element.groups.hasOwnProperty('G2')) {
+
+                        if(element.groups.G2.prof.length != 0) {
+                            teachers.add(element.groups.G2.prof)
+                        }
+
+                    }
+
+                }
+                
+            });
+
+    
+    
+    
+
+            teachers.forEach(element => {
+
+                if(!itemInTeacherList(element)) {
+                    let filterDropdown = document.getElementById("teachers");
+                    let alink =  document.createElement("a");
+                    let el = document.createElement("li");
+    
+                    alink.textContent = element
+                    alink.setAttribute("onClick", "showTeacherName(this)")
+                    alink.classList.add("dropdown-item")
+                    el.appendChild(alink);
+    
+                    
+                    filterDropdown.appendChild(el);
+                }
+
+            });
+
+            let filterDropdown = document.getElementById("teachers");
+            let alink =  document.createElement("a");
+            let el = document.createElement("li");
+
+            alink.textContent = "None"
+            alink.setAttribute("onClick", "showTeacherName(this)")
+            alink.classList.add("dropdown-item")
+            el.appendChild(alink);
+
+            filterDropdown.appendChild(el);
+
+    
+        });
+}
+
+
+
+function clearTeachers() {
+    let filterDropdown = document.getElementById("teachers");
+    
+
+    while(filterDropdown.firstChild) {
+        filterDropdown.removeChild(filterDropdown.firstChild)
+    }
+
+}
+
+
+function itemInTeacherList(item) {
+
+    let filterDropdown = document.getElementById("teachers");
+    
+
+    filterDropdown.childNodes.forEach(element => {
+        if(element.firstChild) {
+            if(element.firstChild.innerHTML == item) {
+                return true
+            }
+        }
+    });
+
+    return false;
+}
